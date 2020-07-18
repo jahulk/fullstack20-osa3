12caf2776a04defa50ed35ef1aeef64190af6fcc
unknown <jarkko.hulkko@japo.fi>
2020-07-18 04:42:36 +0300

    puhelinluettelo full stack

---
+++ build/precache-manifest.eb614b764723a659394497e37f408216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ce04f5010a67aa6471eccc2c989598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28bb68e8cc7dc7c2a754",
+    "url": "/static/css/main.fcda8012.chunk.css"
+  },
+  {
+    "revision": "c816bcf7231dd4c169fa",
+    "url": "/static/js/2.e7311442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7311442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28bb68e8cc7dc7c2a754",
+    "url": "/static/js/main.cdf5ea14.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
